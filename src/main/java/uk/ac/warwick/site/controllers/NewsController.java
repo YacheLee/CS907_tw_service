fa7a6b9eedacf2379cb9873aae89018c21fae04f
mymislife <mymislife@gmail.com>
2019-08-19 11:10:58 +0100

    isTw2016, isTw2017 and isTw2018

--- src/main/java/uk/ac/warwick/site/controllers/NewsController.java
+++ src/main/java/uk/ac/warwick/site/controllers/NewsController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,14 +23,38 @@ public class NewsController {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    public String getQueryForYear(int year){
+        return "timestamp between '" + year + "-01-01' and '" + year + "-12-31' AND ";
+    }
+
     @GetMapping("/api/news")
-    public ArrayNode getNews(@RequestParam String keyword, @RequestParam(defaultValue = "2018") int fromYear, @RequestParam(defaultValue = "2018") int toYear){
+    public ArrayNode getNews(
+            @RequestParam String keyword,
+            @RequestParam(defaultValue = "true") boolean isTw2016,
+            @RequestParam(defaultValue = "true") boolean isTw2017,
+            @RequestParam(defaultValue = "true") boolean isTw2018){
+        List list = new ArrayList();
+        if(isTw2016){
+            list.add(2016);
+        }
+        if(isTw2017){
+            list.add(2017);
+        }
+        if(isTw2018){
+            list.add(2018);
+        }
+
+        if(list.isEmpty()){
+            list.add(3000);
+        }
+
         String sql = "SELECT * FROM appledaily WHERE " +
-                "timestamp between '"+fromYear+"-01-01' and '"+toYear+"-12-31'" +
+                "date_part('year', timestamp) in (:year) " +
                 "AND text LIKE :keyword";
 
         ArrayNode arrayNode = objectMapper.createArrayNode();
         Map<String, Object> map = new HashMap();
+        map.put("year", list);
         map.put("keyword", "%"+keyword+"%");
         namedParameterJdbcTemplate.query(sql, map, rs->{
             arrayNode.add(rs.getString("title"));
